ac6e14edb74f6a08aa38bf31d750083f2c420ced
Maximilian Manser <maximilian.manser@student.englisches-institut.eu>
2026-01-30 11:52:42 +0100

    Code Regeln

--- Regeln.java
+++ Regeln.java
@@ -1,33 +1,56 @@
+public class Regeln {
 
-/**
- * Beschreiben Sie hier die Klasse Regeln.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class Regeln
-{
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
+    public boolean pruefeSieg(char[][] feld, char symbol) {
+        int zeilen = feld.length;
+        int spalten = feld[0].length;
 
-    /**
-     * Konstruktor für Objekte der Klasse Regeln
-     */
-    public Regeln()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
+        // Horizontal
+        for (int i = 0; i < zeilen; i++) {
+            for (int j = 0; j < spalten - 3; j++) {
+                if (feld[i][j] == symbol && feld[i][j+1] == symbol &&
+                    feld[i][j+2] == symbol && feld[i][j+3] == symbol) {
+                    return true;
+                }
+            }
+        }
+
+        // Vertikal
+        for (int i = 0; i < zeilen - 3; i++) {
+            for (int j = 0; j < spalten; j++) {
+                if (feld[i][j] == symbol && feld[i+1][j] == symbol &&
+                    feld[i+2][j] == symbol && feld[i+3][j] == symbol) {
+                    return true;
+                }
+            }
+        }
+
+        // Diagonal ↘
+        for (int i = 0; i < zeilen - 3; i++) {
+            for (int j = 0; j < spalten - 3; j++) {
+                if (feld[i][j] == symbol && feld[i+1][j+1] == symbol &&
+                    feld[i+2][j+2] == symbol && feld[i+3][j+3] == symbol) {
+                    return true;
+                }
+            }
+        }
+
+        // Diagonal ↗
+        for (int i = 3; i < zeilen; i++) {
+            for (int j = 0; j < spalten - 3; j++) {
+                if (feld[i][j] == symbol && feld[i-1][j+1] == symbol &&
+                    feld[i-2][j+2] == symbol && feld[i-3][j+3] == symbol) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+    public boolean pruefeUnentschieden(char[][] feld) {
+        for (int j = 0; j < feld[0].length; j++) {
+            if (feld[0][j] == '.') return false;
+        }
+        return true;
     }
 }
