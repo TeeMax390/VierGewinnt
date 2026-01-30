d20cb7079e95755213d517b8104fd433d7fdfaba
Maximilian Manser <maximilian.manser@student.englisches-institut.eu>
2026-01-30 11:53:15 +0100

    Code Spieler

--- Spieler.java
+++ Spieler.java
@@ -1,33 +1,17 @@
+public class Spieler {
+    private String name;
+    private char symbol;
 
-/**
- * Beschreiben Sie hier die Klasse Spieler.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class Spieler
-{
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Spieler
-     */
-    public Spieler()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
+    public Spieler(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+    public String getName() {
+        return name;
+    }
+
+    public char getSymbol() {
+        return symbol;
     }
 }
